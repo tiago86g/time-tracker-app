434f07d60b89cc6cb4b3bdb6e6050d9d16e7d099
tiago86g <tiago86g@gmail.com>
2021-03-18 20:01:32 +0100

    fix home title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ export const Home = (props: Props) => {
   return (
     <div className="container">
       <h2 className="Home_heading">Select a user to start</h2>
-      <h1 className="home-text">Time tracker home</h1>
+      <h1 className="home-text">Time tracker</h1>
       <div className="clear-data_button-container">
         <button
           className="clear-data_button"
